f0feba0bbc0b8a5d5ea708a803a40ac83a7b6275
MAOCI <themaoci@gmail.com>
2020-09-24 13:22:05 +0200

    Create LessRetardedPhpMyAdminSearch.user.js

---
+++ LessRetardedPhpMyAdminSearch.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name     Less Retarded PhpMyAdmin Search
+// @version  1
+// @author   TheMaoci
+// @grant    none
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @downloadURL https://raw.githubusercontent.com/themaoci/Tempermonkey-Scripts/master/LessRetardedPhpMyAdminSearch.user.js
+// ==/UserScript==
+
+
+if(window.location.href.indexOf("db_search.php") != -1){
+  (function(){
+    var $ = "undefined";
+		function GM_wait()
+    {
+      if(typeof window.jQuery == 'undefined')
+        window.setTimeout(GM_wait,100);
+      else
+      {
+        window.jQuery(function() { 
+          $ = window.jQuery;
+          $( document ).ready(function() {
+          	setTimeout(function(){MainFunction(); }, 2500);
+      		});
+        });
+      }
+    }
+    GM_wait();
+    
+    var SearchLoop;
+		function checkForSearch(){
+    	if($(".noclick").length){
+        $(".noclick").each(function(){ $(this).hide(); });
+      	$(".noclick td a").each(function(){ $(this).parents('tr').show(); });
+        clearInterval(SearchLoop);
+      }
+    }
+    
+    function MainFunction(){
+      console.log("Loaded at PhpMyAdmin - Search Page");
+      SearchLoop = window.setInterval(function(){ checkForSearch($); },500);
+    }
+    
+  })();
+}
